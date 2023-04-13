eb504ac724d7e0e1a43b952e87a8fb7a0614d02b
GuiiAffonso <guiiaffonso.rh@gmail.com>
2023-04-13 14:46:59 -0300

    anotações

--- tipos-e-variaveis/src/App.java
+++ tipos-e-variaveis/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int numero = 5;
+       int numero2 = 10;
+
+       boolean simNao = numero == numero2;
+
+       System.out.println("Nº 1 é igual a Nº 2? " + simNao);
+
+       simNao = numero != numero2;
+
+       System.out.println("Nº 1 é diferente de Nº 2? " + simNao);
+    
+
     }
 }
